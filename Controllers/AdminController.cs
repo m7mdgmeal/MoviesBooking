c9ba68bc05a4e6c8a8002c02ea72dac58539f83b
rezeq1 <rezeqab@ac.sce.ac.il>
2021-01-07 11:10:38 +0200

    Add Mangment Movies view

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MoviesBooking.Models;
 
 namespace MoviesBooking.Controllers
 {
     public class AdminController : Controller
     {
         // GET: Admin
-        public ActionResult Index()
+        public ActionResult MangmentMovies()
         {
             return View();
         }
