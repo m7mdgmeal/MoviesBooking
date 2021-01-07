370ed20ae254a21aab590bd7597b82d05485a179
m7md <m7mdgmeal@gmail.com>
2021-01-07 13:23:48 +0200

    Updating dal

--- DAL/Dal.cs
+++ DAL/Dal.cs
@@ -24,5 +24,33 @@ namespace MoviesBooking.DAL
         public DbSet<Hall> halls { get; set; }
 
 
+        public List<Movie> getMoviesToUser()
+        {
+            //get all recent movies 
+            var movieList = getRecentMovie();
+            // get all avalibale movies
+            movieList = getAvalibleMovies(movieList);
+            //get all movies matching user ageg
+            // movieList = getMatchedAges(movieList);
+            return movieList;
+        }
+        public List<Movie> getRecentMovie()
+        {
+            var movieList = (from x in movies
+                             where x.time.Equals(DateTime.Now.ToString())
+                             select x).ToList<Movie>();
+            return movieList;
+        }
+        public List<Movie> getAvalibleMovies(List<Movie> mv) 
+        {
+           var movieList =(from x in mv
+                           from y in halls
+                           where x.hallId == y.hallId &&
+                           y.seatsNumber > 0
+                           select x).ToList<Movie>();
+            return movieList;
+        }
+
     }
+
 }
