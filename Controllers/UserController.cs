4fd55ea10e43c4254843f870af4a24e4cacd7571
rezeq1 <rezeqab@ac.sce.ac.il>
2021-01-07 17:37:39 +0200

    Add login and register views and ActionResults

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+
 namespace MoviesBooking.Controllers
 {
     public class UserController : Controller
@@ -21,6 +22,52 @@ namespace MoviesBooking.Controllers
             cvm.movies = movies;
             return View(cvm);
         }
+        public ActionResult LoginUsers()
+        {
+            return View();
+        }
+        public ActionResult RegisterUsers()
+        {
+            return View();
+        }
+        public ActionResult AddNewUser(User obj)
+        {
+            Dal dal = new Dal();
+            List<User> exist = (from x in dal.users where x.userName.Contains(obj.userName) select x).ToList<User>();
+            if (exist.Count != 0)
+            {
+                TempData["msg"] = "User already exist !!";
+                TempData["color"] = "red";
+                return View("RegisterUsers");
+            }
+
+            TempData["msg"] = "User created successfully !!!";
+            TempData["color"] = "blue";
+            dal.users.Add(obj);
+            dal.SaveChanges();
+            return View("RegisterUsers");
+        }
+        public ActionResult SingIn(User obj)
+        {
+            Dal dal = new Dal();
+            List<User> exist = (from x in dal.users where x.userName.Equals(obj.userName)
+                                && x.password.Equals(obj.password)
+                                select x).ToList<User>();
+            if (exist.Count == 0)
+            {
+                TempData["msg"] = "Wrong information !!";
+                TempData["color"] = "red";
+                return View("LoginUsers");
+            }
+
+            if(exist[0].userType=="A")
+                return RedirectToAction("MangmentMovies", "Admin");
+            else
+                return RedirectToAction("ShowHowPage", "User"); 
+        }
+
+
+
 
     }
 }
