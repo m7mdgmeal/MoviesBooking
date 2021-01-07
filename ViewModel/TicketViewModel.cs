198d75713e9680e3e29ac3cd88b77554da461def
rezeq1 <rezeqab@ac.sce.ac.il>
2021-01-07 10:40:17 +0200

    Add view models

---
+++ ViewModel/TicketViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using MoviesBooking.Models;
+
+namespace MoviesBooking.ViewModel
+{
+    public class TicketViewModel
+    {
+        public Ticket ticket { get; set; }
+        public List<Ticket> tickets { get; set; }
+    }
+}
+
