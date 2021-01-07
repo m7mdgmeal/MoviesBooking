374496e4a8890c92300b1345c53a4b946705f660
m7md <m7mdgmeal@gmail.com>
2021-01-07 09:42:46 +0200

    Connection to DataBase

--- DAL/Dal.cs
+++ DAL/Dal.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MoviesBooking.Models;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -8,6 +9,20 @@ namespace MoviesBooking.DAL
 {
     public class Dal:DbContext
     {
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<User>().ToTable("usersTbl");
+            modelBuilder.Entity<Movie>().ToTable("moviesTbl");
+            modelBuilder.Entity<Ticket>().ToTable("ticketsTbl");
+            modelBuilder.Entity<Hall>().ToTable("hallsTbl");
+
+        }
+        public DbSet<User> users { get; set; }
+        public DbSet<Movie> movies { get; set; }
+        public DbSet<Ticket> tickets { get; set; }
+        public DbSet<Hall> halls { get; set; }
+
 
     }
 }
