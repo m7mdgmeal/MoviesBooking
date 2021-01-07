6c9e01db9527916b96066a33aea580c2a8919b2d
m7md <m7mdgmeal@gmail.com>
2021-01-07 09:30:15 +0200

    added models

--- DAL/Dal.cs
+++ DAL/Dal.cs
@@ -8,5 +8,6 @@ namespace MoviesBooking.DAL
 {
     public class Dal:DbContext
     {
+
     }
 }
---
+++ Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MoviesBooking.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int movieId { get; set; }
+        public string title { get; set; }
+        public string date { get; set; }
+        public string time { get; set; }
+        public float price { get; set; }
+        public float prePrice { get; set; }
+        public string category { get; set; }
+        public int hallId { get; set; }
+    }
+}
