e28b3a92e6a5af76d283f25737049a7427711f1b
m7md <m7mdgmeal@gmail.com>
2021-01-07 11:12:12 +0200

    fxing bugs

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -13,5 +13,9 @@ namespace MoviesBooking.Controllers
         {
             return View();
         }
+        public ActionResult ShowHowPage()
+        {
+            return View();
+        }
     }
 }
