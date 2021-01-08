efe7b99b236576693fd7c55b0883de248f28d798
m7md <m7mdgmeal@gmail.com>
2021-01-08 07:34:21 +0200

    some chaniging in View

---
+++ ViewModel/SeatsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using MoviesBooking.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MoviesBooking.ViewModel
+{
+    public class SeatsViewModel
+    {
+        public Movie movie { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
